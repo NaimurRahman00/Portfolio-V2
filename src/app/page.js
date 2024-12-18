299ffcec53118c67a142a696849af571a48a5f2e
Naimur00 <aminaim4u@gmail.com>
2024-12-18 17:27:38 +0600

    add: service section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/blocks/HeroSection";
+import { Service } from "@/components/blocks/Service";
 
 
 export default function Home() {
   return (
     <>
       <HeroSection/>
+      <Service/>
     </>
 
   );
