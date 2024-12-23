3e25e8a6a9965c2cefe2cd28e7b4ba019d9c53b8
Naimur00 <aminaim4u@gmail.com>
2024-12-23 15:40:05 +0600

    update: skills section

--- src/components/blocks/SkillsSection.jsx
+++ src/components/blocks/SkillsSection.jsx
@@ -6,40 +6,72 @@ import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
 export function SkillsSection() {
   return (
     (<div
-      className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-      <InfiniteMovingCards items={testimonials} direction="right" speed="slow" />
+      className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.2] bg-grid-black/[0.2] items-center justify-center relative overflow-hidden">
+      <h4 className="text-3xl lg:text-5xl lg:leading-tight max-w-5xl mx-auto text-center text-white font-medium mb-2 lg:mb-8">
+        All that technology I know
+      </h4>
+      <InfiniteMovingCards items={skills} direction="right" speed="slow" />
+      <InfiniteMovingCards items={tools} direction="left" speed="slow" />
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
     </div>)
   );
+
+
 }
 
-const testimonials = [
+const skills = [
   {
-    quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Charles Dickens",
-    title: "A Tale of Two Cities",
+    logo: '/html.png'
   },
   {
-    quote:
-      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    name: "William Shakespeare",
-    title: "Hamlet",
+    logo: '/css.png'
   },
   {
-    quote: "All that we see or seem is but a dream within a dream.",
-    name: "Edgar Allan Poe",
-    title: "A Dream Within a Dream",
+    logo: '/tailwind.png'
   },
   {
-    quote:
-      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-    name: "Jane Austen",
-    title: "Pride and Prejudice",
+    logo: '/bootstrap-framework.png'
   },
   {
-    quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
-    title: "Moby-Dick",
+    logo: '/js.png'
+  },
+  {
+    logo: '/ts.png'
+  },
+  {
+    logo: '/react.png'
+  },
+  {
+    logo: '/nextjs.512x309.png'
   },
 ];
+
+const tools = [
+  {
+    logo: '/firebase.png'
+  },
+  {
+    logo: '/npm.png'
+  },
+  {
+    logo: '/mongodb.png'
+  },
+  {
+    logo: '/figma.png'
+  },
+  {
+    logo: '/git.png'
+  },
+  {
+    logo: '/express.png'
+  },
+  {
+    logo: '/github.png'
+  },
+  {
+    logo: '/vscode.png'
+  },
+  {
+    logo: '/node.png'
+  },
+]
--- src/components/ui/infinite-moving-cards.jsx
+++ src/components/ui/infinite-moving-cards.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 export const InfiniteMovingCards = ({
@@ -70,31 +71,19 @@ export const InfiniteMovingCards = ({
         )}>
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
+            className="size-[120px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 p-5 flex items-center justify-center"
             style={{
               background:
                 "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
             }}
             key={item.name}>
-            <blockquote>
               <div
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"></div>
-              <span
-                className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
-                {item.quote}
-              </span>
-              <div className="relative z-20 mt-6 flex flex-row items-center">
-                <span className="flex flex-col gap-1">
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.name}
-                  </span>
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.title}
-                  </span>
-                </span>
+
+              <div className="relative z-20 flex flex-row items-center">
+                <Image src={item.logo} width={120} height={120} alt="skills" />
               </div>
-            </blockquote>
           </li>
         ))}
       </ul>
